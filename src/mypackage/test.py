3a0b8ea98e1ff03cf0c78924f85835d597000df9
nemoboi <148422945+nemoboi@users.noreply.github.com>
2023-11-27 14:20:21 +0100

    updated version test.py incl CLI
    added CLI with parameters estimate, deadline, area, path, cond
    deadline still needs conversion str->date

--- src/mypackage/test.py
+++ src/mypackage/test.py
@@ -1,17 +1,19 @@
-	#!/usr/bin/env python3
+#!usr/bin/env python3
 # Module containing the data transformation
 from datetime import date, datetime
 import click
 
 @click.command()
-@click.option('--estimate', t help='Estimated runtime of the program in hours')
-@click.option('--area', help='Local area code as given in x.file') #default implementation
-@click.option('--deadline', help='latest time when the program should be finished in "YYYY-MM-DD hh:mm:ss"')
+@click.option('--estimate', type=float, help='Estimated runtime of the program in hours')
+@click.option('--deadline', type=str, help='latest date when the program should be finished in "YYYY-MM-DD"') #maybe allow for time later... date works for now
+@click.option('--area', type=str, help='Local ara code as given in x.file') #default implementation
+@click.option('--path', type=str, help='Path to your application')
+@click.option('--cond', type=str, default=None, help='conditions for execution')
 
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+def hello(estimate, deadline, area, path, cond):
+    print(f"Input: estimate {estimate}, deadline {deadline}, area {area}, path {path}, conditions {cond}")
+    print("Joke's on you this doesn't do anything.")
+    #deadline still needs to be converted into date.fromisoformat(deadline)
 
 if __name__ == '__main__':
     hello()
